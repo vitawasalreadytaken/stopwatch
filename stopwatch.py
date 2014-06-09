446252ffdd7090a1093240e070406de880b706d0
Vita Smid <me@ze.phyr.us>
2014-06-09 11:56:02 +0200

    Added silent mode

--- stopwatch.py
+++ stopwatch.py
@@ -4,11 +4,15 @@ logger = logging.getLogger('stopwatch')
 
 
 class Stopwatch(object):
-	def __init__(self, comment):
+	def __init__(self, comment = None):
 		self.comment = comment
 
 	def __enter__(self):
 		self.T0 = time.time()
+		self.elapsed = 0
+		return self
 
 	def __exit__(self, exc_type, exc_val, exc_tb):
-		logger.debug('"{:s}" done in {:.3f} seconds.'.format(self.comment, time.time() - self.T0))
+		self.elapsed = time.time() - self.T0
+		if self.comment is not None:
+			logger.debug('"{:s}" done in {:.3f} seconds.'.format(self.comment, self.elapsed))
